956fc443cd4f68ea25056cbb8771336dedfd141a
Jennifer <jenndestes@gmail.com>
2021-03-11 23:36:06 -0600

    commit tree bf6db471f8860d6adb08aa6368e850169ba6f7fc

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,15 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+    def change
+        create_table :students do |t|
+            t.string :name
+        # sql = <<-SQL
+        # CREATE TABLE IF NOT EXISTS students (
+        #     name TEXT,
+        #     id INTEGER PRIMARY KEY
+        # )
+        # SQL
+        # ActiveRecord::Base.connection.execute(sql)
+        end
+    end
 end
